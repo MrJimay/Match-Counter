ecb8450520f7c550998e6b84598e35d0be5897b2
Jimmy Huang <MrJimay@users.noreply.github.com>
2016-08-04 20:44:41 +1000

    Print matches to output file when taking in a file
    Previously this only worked when taking in a single regex

--- countMatches.pl
+++ countMatches.pl
@@ -3,58 +3,72 @@ use strict;
 use warnings;
 use Term::ANSIColor qw(:constants);
 
+# Name of the file we write the results to
 my $output_file = "output.txt";
+
+# Used for formatting and storing results if a file is passed in instead of a single regex
+my @output_array;
+
 my $argument_error_message = "Expecting at least 2 arguments. Correct usage is ./countMatches.pl <csv file> <regex pattern> [column #] [flag] where [] arguments are optional.\n Type ./countMatches.pl help for help\n";
-my $col_num = 0;
+
+# Store info passed in from optional command line arguments
+my $col_num = 0; # default column to search through is the first column
 my $flag;
-# whether to read a file or a single regex pattern
+
+# Store whether to read a file or a single regex pattern
 # 0 - regex pattern
 # 1 - file
-my $read_file = 0;
-open(my $output, '>', $output_file);
+my $is_file_read = 0;
 
+# Take in the first argument as the name of the csv file to process
 my $input_file = $ARGV[0] or die $argument_error_message;
+
+# Open the output file. Needs global scope so subroutines can access it
+open(my $output, '>', $output_file);
 
 if ($input_file eq "help") {
 	print_help();
 } else {
+	process_optional_args();
+
 	my $pattern = $ARGV[1] or die $argument_error_message;
 
 	# Check whether a file or a single pattern was inputted
 	if (-f $pattern) {
-		$read_file = 1;
+		$is_file_read = 1;
 	}
-	
-	my $optional_arg = $ARGV[2];
-	my $optional_arg_two = $ARGV[3];
 
-	read_optional_args($optional_arg, $optional_arg_two);
-
-	if ($read_file == 1) {
-		open(my $regex, '<', $pattern) or die "Failed to read regex file. Please see help for more info.\n";
-		print $output "Total: ", match_regex(".*"), "\n";
-		while (my $line = <$regex>) {
-			chomp $line;
-			print $output "$line: ", match_regex($line), "\n";
-		}
-		print GREEN, "Success! Check your output.txt file\n", RESET;
+	if ($is_file_read == 1) {
+		process_regex_file($pattern);
 	} else {
 		my $sum = match_regex($pattern);
 		print_result($sum);	
 	}
 
-	# Open the output file if -o flag was set
-	if (defined $flag) {
-		if ($flag eq '-o') {
-			system("open", $output_file);
-		}
-	}	
+	open_output_if_flag_set($output_file);
 }
 
+# Takes in a file name and treats each line in the file as regex to match against the input file
+sub process_regex_file {
+	open(my $regex, '<', $_[0]) or die "Failed to read regex file. Please see help for more info.\n";
+	print "Counting total...\n";
+	print $output "Total: ", match_regex(""), "\n";
+	while (my $line = <$regex>) {
+		chomp $line;
+		print "Counting matches for $line...\n";
+		push(@output_array, "\n");
+		push(@output_array, "=== Results matching $line ===\n");
+		print $output "$line: ", match_regex($line), "\n";	
+	}
+	print $output "\n@output_array";
+	print GREEN, "Success! Check your output.txt file\n", RESET;
+}
+
+# Takes in regex and returns the number of matches in the input file
 sub match_regex {
 	open(my $data, '<', $input_file) or die "Could not open '$input_file' $!\n";
 	my $sum = 0;
-	# read csv file
+	# read csv file line by line
 	while (my $line = <$data>) {
 	  chomp $line;
 	  # get the columns of the csv into an array
@@ -65,19 +79,42 @@ sub match_regex {
 	  	die "Column number $col_num not found.\n";
 	  }
 	  if($fields[$col_num] =~ $_[0]) {
-	  	if ($read_file == 0) {
+	  	if ($is_file_read == 0) {
+	  		# if we're reading single regex, just print to the file
 	  	  	print $output "$fields[$col_num] \n";
+	  	} else {
+	  		# otherwise if we're reading multiple regex, store results in an array for processing
+	  		# this allows us to put the important counts at the top of the output file
+	  		if (!$_[0] eq "") {
+	  			push(@output_array, $fields[$col_num]);
+	  		}
 	  	}
 	  	$sum += 1;
 	  }
-	}	
+	}
 	close($data);
 	return $sum;
 }
 
-# Reads in the optional args 
-# and sets the global variables $col_num and $flag
-sub read_optional_args {
+# Reads in the optional args
+# then sets the global variables $col_num and $flag
+sub process_optional_args {
+	my $optional_arg = $ARGV[2];
+	my $optional_arg_two = $ARGV[3];
+	set_optional_args($optional_arg, $optional_arg_two);
+}
+
+# Opens the file passed in if -o flag was set
+sub open_output_if_flag_set {
+	if (defined $flag) {
+		if ($flag eq '-o') {
+			system("open", $_[0]);
+		}
+	}
+} 
+
+# Sets the global variables $col_num and $flag
+sub set_optional_args {
 	foreach my $a (@_) {
 		if (defined $a) {
 			if ($a =~ '[0-9]+') {
@@ -89,6 +126,7 @@ sub read_optional_args {
 	}
 }
 
+# Print out a cool looking help screen
 sub print_help {
 	print "===================================\n";
 	print "Jimmy's pattern matching script!\n";
@@ -104,7 +142,7 @@ sub print_help {
 	print " -> ", CYAN, "<txt file name>\n", RESET, "Matches regex patterns inside this txt file. Each regex pattern must be separated with a new line.\n";
 	print "\n";
 	print "Optional arguments:\n";
-	print " -> ", CYAN, "-o\n", RESET, "Open a .txt file after the search with a list of results that matches the input pattern\n";
+	print " -> ", CYAN, "-o\n", RESET, "Open a .txt file after the search with a list of results that matches the input pattern.\n";
 	print " -> ", CYAN, "<column #>\n", RESET, "Specify a column number to search through, otherwise searches through the first column.\n";
 	print "\n";
 }
